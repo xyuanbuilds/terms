b25769ca0ac4e445424415e21506bcd193fcdb9e
xyuanbuilds <1076849402@qq.com>
2022-05-11 09:38:52 +0800

    feat: cache rm

---
+++ src/implement/basic/prototype.ts
@@ -0,0 +1,29 @@
+/**
+ * prototype & __proto__
+ * 
+ * 
+ */
+interface P {
+	readonly prototype: any;
+}
+/**
+ * Instances of
+ * 遍历原型链，直到 __protp__ 指向当前构造对象，或null
+ * @param A 
+ * @param B 
+ * @returns  
+ */
+export function instanceOf(A: any, B: P) {
+	A = A.__proto__;
+	while (true) {
+		if (A === null) {
+			return false;
+		}
+		if (A === B.prototype) {
+			return true;
+		}
+		A = A.__proto__;
+	}
+}
+// const res = instanceOf("1", String);
+// console.log(res);
